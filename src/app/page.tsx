2bc448e13bdc526174285231ea91e06f938168e6
Matthew Garcia <gmatthew2765@gmail.com>
2026-01-10 14:37:46 -0800

    integrated tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,7 @@ import { Nav } from "./components/nav/Nav";
 
 export default function Home() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-      width: '100%',
-      overflow: 'hidden',
-      paddingTop: '76px' // Account for fixed nav
-    }}>
+    <div className="flex">
       <Nav />
       <Body />
     </div>
